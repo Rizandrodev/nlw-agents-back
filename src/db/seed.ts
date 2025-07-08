1d1a122274b3c9b8cc421bf1ff8f00a497275f02
Rizdev <Carvalhorizandro@gmail.com>
2025-07-08 22:43:08 +0100

    db:config docker with postgresql  drizzle

---
+++ src/db/seed.ts
@@ -0,0 +1,20 @@
+import { reset, seed } from 'drizzle-seed'
+import { schema } from './schema/index.ts'
+import { db, sql } from './connection.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine((f) => ({
+  rooms: {
+    count: 20,
+    columns: {
+      name: f.companyName(),
+      description: f.loremIpsum(), // Corrigido de "lorenIpsun"
+    }
+  }
+}))
+
+await sql.end()
+
+// biome-ignore lint/suspicious/noConsole: only for development
+console.log("✅ Database Seeded")
