c90eb719c5ed59fe59906f1d5541c7f389f340a3
Rizdev <Carvalhorizandro@gmail.com>
2025-07-12 10:41:28 +0100

    feat: Gemini SDK Add audio transcription and text embedding capabilities

--- src/http/routes/upload-audio.ts
+++ src/http/routes/upload-audio.ts
@@ -1,6 +1,6 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import z from 'zod/v4'
-import { transcribeAudio } from '../services/gemini.ts';
+import { generateEmbeddings, transcribeAudio } from '../services/gemini.ts';
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post('/rooms/:roomId/audio', {
@@ -11,7 +11,7 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
       // Nenhum body em JSON aqui, pois estamos lidando com multipart/form-data
     },
   }, async (request, reply) => {
-    const { roomId } = request.params
+    //const { roomId } = request.params
     const audio = await request.file();
 
     if (!audio) {
@@ -19,13 +19,15 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
     }
 
     const AudioBuffer=await audio.toBuffer()
-    const audioAsBase64=AudioBuffer.toString('base64')
+      const audioAsBase64=AudioBuffer.toString('base64')
 
-    const transcription = await transcribeAudio(
-      audioAsBase64,
-      audio.mimetype
-    )
-    return {transcription }
+      const transcription = await transcribeAudio(
+        audioAsBase64,
+        audio.mimetype
+      )
+
+    const embeddings=await generateEmbeddings(transcription)
+    return {transcription,embeddings}
     
   });
 };
--- src/http/services/gemini.ts
+++ src/http/services/gemini.ts
@@ -33,3 +33,18 @@ export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
   }  
   return result;
 }
+
+export async function generateEmbeddings(text:string){
+  const response=await gemini.models.embedContent({
+    model:'text-embedding-004',
+    contents:[{text}],
+    config:{
+      taskType:'RETRIEVAL_DOCUMENT'
+    }
+  })
+
+  if(!response.embeddings?.[0].values){
+      throw new Error('nao foi possivel gerar embeddings')
+  }
+  return response.embeddings[0].values
+}
